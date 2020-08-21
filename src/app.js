e940b7d8ea3d7c50d350d3df6e61b0d7cfd0953c
Andrés Villalobos <agmvilla@gmail.com>
2020-08-21 01:17:27 -0300

    Round 1: create, list, delete (no test cases at all)

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid, isUuid } = require("uuidv4")
 
 // const { v4: uuid } = require('uuid');
 
@@ -10,20 +11,51 @@ app.use(cors());
 
 const repositories = [];
 
+
+function validateRepoId(request, response, next) {
+  const { id } = request.params
+
+  if( !isUuid(id) ) {
+    return response.status(400).json({ error: 'Invalid repo ID' })
+  }
+
+  return next()
+}
+
+app.use( "/repositories/:id", validateRepoId );
+
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+
+  console.log(title);
+  console.log(url);
+  console.log(techs);
+
+  const repo = { id: uuid(), title, url, techs, likes: '0' }
+  repositories.push(repo)
+
+  return response.json(repo)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex( repo => repo.id == id);
+  if( repoIndex < 0 ) {
+    return response.status(400).json({error: "Repo not found"})
+  }
+
+  repositories.splice( repoIndex, 1 )
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
